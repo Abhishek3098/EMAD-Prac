66afd1576863f7208a0557e08858a0d4e7492e09
Gaurav Bothra <gaurav.ma1503@ict.gnu.ac.in>
2018-01-06 18:10:15 +0530

    Initial Commit

--- Assignment/apps/Tasks001/android/native/assets/www/default/worklight/checksum.js
+++ Assignment/apps/Tasks001/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1515239998607,"machine":"DESKTOP-9BPGVR5","checksum":176705389};
-/* Date: Sat Jan 06 17:29:58 IST 2018 */
+var WL_CHECKSUM = {"date":1515240626467,"machine":"DESKTOP-9BPGVR5","checksum":4038652143};
+/* Date: Sat Jan 06 17:40:26 IST 2018 */
