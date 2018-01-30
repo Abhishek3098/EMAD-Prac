f5c309bdc5fbe7698f7a11643160040aa80dccd2
Gaurav Bothra <gaurav.ma1503@ict.gnu.ac.in>
2018-01-30 17:35:12 +0530

    Done 30/1/18 work

--- MailApp/server/java/com/mail/mailus/PasswordMail.java
+++ MailApp/server/java/com/mail/mailus/PasswordMail.java
@@ -13,8 +13,8 @@ import javax.mail.internet.MimeMessage;
 				public boolean sendMail(String to,String msgs,String s) throws MessagingException
 		{
 			String host="smtp.gmail.com";
-String username="yourmailid";//emailid
-			String password="yourpassword";//emailid password
+String username="yourmailid";
+			String password="yourpassword";
 			String subject=s;
 String body = msgs;
 	 Properties properties = new Properties();
@@ -23,31 +23,19 @@ String body = msgs;
      properties.put("mail.smtp.auth", "true");
      properties.put("mail.smtp.starttls.enable", "true");
      properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-//*** BEGIN CHANGE
      properties.put("mail.smtp.user", username);
-
-     // creates a new session, no Authenticator (will connect() later)
      Session session = Session.getDefaultInstance(properties);
-//*** END CHANGE
-
-     // creates a new e-mail message
      Message msg = new MimeMessage(session);
-
      msg.setFrom(new InternetAddress(username));
      InternetAddress[] toAddresses = { new InternetAddress(to) };
      msg.setRecipients(Message.RecipientType.TO, toAddresses);
      msg.setSubject(subject);
      msg.setSentDate(new Date());
-     // set plain text message
      msg.setText(body);
-
-//*** BEGIN CHANGE
-     // sends the e-mail
      Transport t = session.getTransport("smtp");
      t.connect(username, password);
      t.sendMessage(msg, msg.getAllRecipients());
      t.close();
-//*** END CHANGE
 			return true;
 		}
 				public boolean sendM(String to, String msg, String subject){
