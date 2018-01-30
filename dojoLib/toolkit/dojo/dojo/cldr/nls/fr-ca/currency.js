2f1a4b6e53b58ef0ba0d7f7c108bd0a82c400d36
Gaurav Bothra <gaurav.ma1503@ict.gnu.ac.in>
2018-01-30 10:00:34 +0530

    Done upto 4

--- dojoLib/toolkit/dojo/dojo/cldr/nls/fr-ca/currency.js
+++
@@ -1,9 +0,0 @@
-define(
-//begin v1.x content
-{
-	"CNY_symbol": "CN¥",
-	"WST_symbol": "WST",
-	"CAD_symbol": "$"
-}
-//end v1.x content
-);
--- dojoLib/toolkit/dojo/dojo/cldr/nls/mt/currency.js
+++
@@ -1,9 +0,0 @@
-define(
-//begin v1.x content
-{
-	"EUR_displayName": "Ewro",
-	"MTL_displayName": "Lira Maltija",
-	"XXX_displayName": "Munita Mhux Magħrufa jew Mhux Valida"
-}
-//end v1.x content
-);
